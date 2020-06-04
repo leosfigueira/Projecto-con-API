0885a48ec1dcb118c46a84133af851790e80ee6b
Leo Figueira <leolp.16@gmail.com>
2020-06-04 17:25:37 -0300

    Agrego columna de casos diarios en covid

--- js/covid.js
+++ js/covid.js
@@ -1,11 +1,10 @@
+
 function getdata(string) {
   console.log(string);
   let countryselected = string; //$("#idpais").val();
   axios
     .get(
-      "https://cors-anywhere.herokuapp.com/https://api.covid19api.com/total/country/" +
-      countryselected +
-      ""
+      "https://api.covid19api.com/total/country/" + countryselected + ""
       //"https://api.covid19api.com/countries"
       //"https://api.covid19api.com/all"
       // "https://api.covid19api.com/dayone/country/" +
@@ -18,18 +17,35 @@ function getdata(string) {
       //console.log(response);
       let countries = response.data;
       let output = "";
+      let totalcasosdeldia = 0;
+      let diaanterior = 0;
+      let casosdeldia = 0;
+      var dia = 0;
       $.each(countries, (index, country) => {
+        totalcasosdeldia = country.Confirmed;
+        //console.log("total de casos confirmados nuevos " + totalcasosdeldia);
+        //console.log("antes de validar casosdeldiaanterior  " + diaanterior);
+        //console.log("antes de validar casosdeldia  " + casosdeldia);
+        if (diaanterior != totalcasosdeldia) {
+          casosdeldia = totalcasosdeldia - diaanterior;
+        }
+        diaanterior = totalcasosdeldia;
+        //console.log("casos confirmados guardados del dia  " + totalcasosdeldia);
+        //console.log("casosdeldiaanterior  " + diaanterior);
+        //console.log("casosdeldia  " + casosdeldia);
         output += `
             <tr>
                         <td scope="row">${index}</td>
                         <td id="country">${country.Country}</td>
                         <td id="date">${country.Date}</td>
+                        <td id="cases">${casosdeldia}</td>
                         <td id="cases">${country.Confirmed}</td>
                         <td id="cases">${country.Active}</td>
                         <td id="cases">${country.Recovered}</td>
                         <td id="cases">${country.Deaths}</td>
                       </tr>
           `;
+        // cal(dia1,casosconfirmed);
       });
       $("#tbody").html(output);
     });
